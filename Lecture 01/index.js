9dcc11058c0088e0b4eed8ab4c5002d18873b0f6
gerejakajal <gerejakajal207@gmail.com>
2025-08-23 21:53:33 +0530

    Added lecture 01

---
+++ Lecture 01/index.js
@@ -0,0 +1,8 @@
+console.log("Hello world")
+console.warn("Warning")
+console.error("Not found")
+function callFunc(){
+    if(authorized)
+    console.log("Button clicked!!")
+    console.log("Function call is done from inline but funcn is written inside external")
+}
